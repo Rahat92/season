5d4b7a4cb736d0e9bbaf1dd8ca428c8b0de35b82
Kamrul Hasan <khrahat92@gmail.com>
2022-03-06 21:51:25 +0600

    first commit ever

---
+++ src/Spinner.js
@@ -0,0 +1,11 @@
+const Spinner = ({message})=>{
+    return(
+            <div class="ui active dimmer">
+                <div class="ui text loader">{message}</div>
+            </div>
+    )
+}
+Spinner.defaultProps={
+    message:"Loading..."
+}
+export default Spinner;
